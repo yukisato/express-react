935a2257c20330904cc105a53fd8266ed760f389
Yuki Sato <yuki.sato.dev@gmail.com>
2020-12-29 23:46:25 -0800

    fix(package.json): use yarn to build packages instead because npm@7 has npm script execution issue
    See official issue: https://github.com/npm/cli/issues/1904

--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ npm install --only-dev && npm install
 
 # Build the backend project
 cd ${CURRENT_DIR}/packages/backend
-npm run build
+npx yarn build
 
 # Build the frontend project
 cd ${CURRENT_DIR}/packages/frontend
-npm run build
+npx yarn build
